e8bffe8c8d1c3212ae3681014b10e8bff91061fc
seo1v <165138611+seo1v@users.noreply.github.com>
2024-08-24 01:45:58 +0900

    useEffect 추가

--- src/components/GroupPage.jsx
+++ src/components/GroupPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../styles/GroupPage.css";
 import logo from "../assets/logo.svg";
 import groupimg from "../assets/groupimg.svg";
